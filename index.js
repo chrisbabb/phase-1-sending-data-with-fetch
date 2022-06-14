6990dada948727093fdff43484e55b4145c06e9f
Christopher Babb <chris@appsoftdev.com>
2022-06-13 19:01:09 -0700

    commit tree c8d7525591d725314b975b5252360b88325d9ce3

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const postData = {
+        name: `${userName}`,
+        email: `${userEmail}`,
+    }
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(postData),
+    };
+
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(data => data.json())
+    .then(parsed => document.body.append(parsed.id))
+    .catch(error => document.body.append(error))
+}
